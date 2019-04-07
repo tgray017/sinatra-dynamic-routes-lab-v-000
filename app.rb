8d9a55ef11512551933a7c59e762c129a3f30515
Thomas Gray <tgray017@gmail.com>
2019-04-07 16:40:29 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -13,9 +13,8 @@ class App < Sinatra::Base
   get '/say/:number/:phrase' do
     @number = params[:number].to_i
     @phrase = params[:phrase]
-    raise @number.inspect
-    @number.times do
-      "#{@phrase.to_s}"
+    <%@number.times do%>
+      <=%"#{@phrase.to_s}"
     end
   end
 
