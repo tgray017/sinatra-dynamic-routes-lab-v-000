cff16f5336941e65781e0b94aa59af256aae3bfd
Thomas Gray <tgray017@gmail.com>
2019-04-07 16:27:38 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,5 +5,9 @@ class App < Sinatra::Base
   get '/reversename/:name' do
     "#{params[:name].reverse}"
   end
+  
+  get '/square/:number' do
+    "#{params[:number].to_i*params[:number"
+  end
 
 end
